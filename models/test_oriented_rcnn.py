0fef9e590cb97f61bdcf3ceebb96be3c0c7b71fc
Kristupas Gaidys <78504062+kristupas-g@users.noreply.github.com>
2024-04-05 14:10:12 +0000

    update demo image path in test

--- models/test_oriented_rcnn.py
+++ models/test_oriented_rcnn.py
@@ -1,7 +1,7 @@
 from oriented_rcnn import Detector
 import cv2
 
-img = cv2.imread("/workspaces/prototype/test/demo.jpg")
+img = cv2.imread("/workspaces/prototype/demo.jpg")
 
 detector = Detector("/workspaces/prototype/deployment_files/oriented_rcnn")
 
--- models/test_rsinet.py
+++ models/test_rsinet.py
@@ -1,7 +1,7 @@
 from rsinet import SuperResolver
 import cv2
 
-img = cv2.imread("/workspaces/prototype/test/demo.jpg", 1)
+img = cv2.imread("/workspaces/prototype/demo.jpg", 1)
 
 super_resolver = SuperResolver(
     "/workspaces/prototype/deployment_files/sr/model.onnx"
