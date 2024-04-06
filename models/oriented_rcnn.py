b5ea5432b7e9edd3691e1da2dbd60ff5e1fba4a3
Kristupas Gaidys <78504062+kristupas-g@users.noreply.github.com>
2024-04-06 14:52:23 +0000

    visualize output

--- models/oriented_rcnn.py
+++ models/oriented_rcnn.py
@@ -1,9 +1,19 @@
 from mmdeploy_runtime import RotatedDetector
+import numpy as np
+from matplotlib.patches import Polygon
+import matplotlib.pyplot as plt
+import io
+from PIL import Image
 
 class Detector:
     def __init__(self, model_path, device='cpu'):
         self.model_path = model_path
         self.device = device
+
+        self.classes = {
+            4: {'name': 'Class 4', 'color': 'blue'},
+            5: {'name': 'Class 5', 'color': 'green'}
+        }
 
         self.detector = RotatedDetector(
             model_path = self.model_path,
@@ -13,3 +23,60 @@ class Detector:
 
     def run(self, img):
         return self.detector(img)
+
+    def visualize_results(self, image, bboxes, labels, score_threshold=0.5, thickness=2):
+        fig, ax = plt.subplots()
+        ax.imshow(image)
+
+        for bbox, label in zip(bboxes, labels):
+            xc, yc, w, h, ag, score = bbox
+            
+            if score >= score_threshold and label in self.classes:
+                color = self.classes[label]['color']
+                
+                wx, wy = w / 2 * np.cos(ag), w / 2 * np.sin(ag)
+                hx, hy = -h / 2 * np.sin(ag), h / 2 * np.cos(ag)
+                p1 = (xc - wx - hx, yc - wy - hy)
+                p2 = (xc + wx - hx, yc + wy - hy)
+                p3 = (xc + wx + hx, yc + wy + hy)
+                p4 = (xc - wx + hx, yc - wy + hy)
+                poly = np.array([p1, p2, p3, p4], dtype=np.float64)
+                
+                rect = Polygon(poly, closed=True, facecolor='none', edgecolor=color, linewidth=thickness, alpha=0.7)
+                ax.add_patch(rect)
+                
+        plt.subplots_adjust(left=0, right=1, top=1, bottom=0, wspace=0, hspace=0)
+        ax.margins(0,0)
+        ax.xaxis.set_major_locator(plt.NullLocator())
+        ax.yaxis.set_major_locator(plt.NullLocator())
+
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
+
+        buf.seek(0)  
+        plt.close()
+        return Image.open(buf)
+
+
+    def generate_legend():
+        fig, ax = plt.subplots()
+        for class_id, attributes in self.classes.items():
+            ax.plot([], [], color=attributes['color'], label=attributes['name'])
+        legend = ax.legend(frameon=False)
+        ax.set_axis_off()
+
+        buffer = io.BytesIO()
+        plt.savefig(
+            buffer,
+            format='png',
+            bbox_inches=legend
+                    .get_window_extent()
+                    .transformed(fig.dpi_scale_trans.inverted())
+                    .expanded(1.2, 1.2),
+            transparent=False
+        ) 
+
+        plt.close(fig) 
+        buffer.seek(0) 
+
+        return buffer.getvalue()
--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from models.oriented_rcnn import Detector
+
 from PIL import Image
 import base64
 import io
@@ -13,6 +15,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="views")
 
+detector = Detector(model_path="/workspaces/prototype/deployment_files/oriented_rcnn")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
@@ -24,9 +27,10 @@ async def process_image(request: Request, image: UploadFile = File(...)):
     contents = await image.read()
 
     with Image.open(io.BytesIO(contents)) as img:
-        img = img.rotate(90)
+        bboxes, classes = detector.run(img)
+        img_with_results = detector.visualize_results(img, bboxes, classes)
         img_io = io.BytesIO()
-        img.save(img_io, format="PNG")
+        img_with_results.save(img_io, format="PNG")
         img_io.seek(0)
         base64_image = base64.b64encode(img_io.getvalue()).decode("utf-8")
 
